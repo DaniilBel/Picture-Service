d3638d70eb844d84179312d188b9741ddf9b1040
dan <danya.belyaev@gmail.com>
2024-06-21 08:16:57 +0300

    Fix

--- src/main/java/org/project/pictureservice/PictureServiceApplication.java
+++ src/main/java/org/project/pictureservice/PictureServiceApplication.java
@@ -1,11 +1,8 @@
 package org.project.pictureservice;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
--- src/main/java/org/project/pictureservice/service/listener/KafkaListeners.java
+++
@@ -1,9 +0,0 @@
-package org.project.pictureservice.service.listener;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaListeners {
-
-
-}
