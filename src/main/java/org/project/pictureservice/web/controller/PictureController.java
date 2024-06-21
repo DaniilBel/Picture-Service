a15a9a7abbd7460cc253353825161d39074b500d
dan <danya.belyaev@gmail.com>
2024-06-21 06:07:54 +0300

    Add workflow.yml

--- src/main/java/org/project/pictureservice/web/controller/PictureController.java
+++ src/main/java/org/project/pictureservice/web/controller/PictureController.java
@@ -14,7 +14,6 @@ import org.project.pictureservice.web.mappers.PictureMapper;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
