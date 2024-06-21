c1f256ab0f555297221124a177ca2c3c02eff2e8
dan <danya.belyaev@gmail.com>
2024-06-21 08:19:13 +0300

    Add kafka

--- src/main/java/org/project/pictureservice/PictureServiceApplication.java
+++ src/main/java/org/project/pictureservice/PictureServiceApplication.java
@@ -1,6 +1,5 @@
 package org.project.pictureservice;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
